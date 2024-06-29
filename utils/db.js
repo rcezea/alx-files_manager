36e38316e3ef20147601f84e512a858f584a7e7d
Richard <rcezea007@gmail.com>
2024-06-30 00:12:04 +0100

    reverted and made env variables global

--- utils/db.js
+++ utils/db.js
@@ -1,39 +1,44 @@
-import { MongoClient, ObjectId } from 'mongodb';
+const { MongoClient } = require('mongodb');
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
-const dbName = process.env.DB_DATABASE || 'files_manager';
+const database = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${host}:${port}`;
 
 class DBClient {
   constructor() {
-    this.client = MongoClient(url, { useUnifiedTopology: true });
-    this.connect();
-    this.connected = false;
-  }
-
-  async connect() {
-    await this.client.connect();
-    this.connected = true;
-    this.db = this.client.db(dbName);
-    this.fileCollection = this.db.collection('files');
-    this.userCollection = this.db.collection('users');
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client.connect()
+      .then((client) => {
+        this.db = client.db(database);
+        this.userCollection = this.db.collection('users');
+        this.fileCollection = this.db.collection('files');
+      })
+      .catch((err) => {
+        console.error(err);
+        return err;
+      });
   }
 
   isAlive() {
-    return this.connected;
+    return !!this.db;
   }
 
   async nbUsers() {
-    const usersCollection = this.db.collection('users');
-    const noOfCollections = await usersCollection.countDocuments({});
-    return noOfCollections;
+    if (!this.isAlive()) {
+      return 0;
+    }
+    return this.userCollection.countDocuments();
   }
 
   async nbFiles() {
-    const filesCollection = this.db.collection('files');
-    const noOfFiles = await filesCollection.countDocuments({});
-    return noOfFiles;
+    if (!this.isAlive()) {
+      return 0;
+    }
+    return this.fileCollection.countDocuments();
   }
 }
 
