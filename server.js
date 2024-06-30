b93514ba75d8a1d00226c6ab50e578d02814e47c
Richard <rcezea007@gmail.com>
2024-06-30 14:31:02 +0100

    updated documentation

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import express from 'express';
-// import dotenv from 'dotenv';
 import router from './routes/index';
 
-// dotenv.config();
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 const app = express();
