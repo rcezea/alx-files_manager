55286496180aaea31ab7d2eaaf2efb4908339b0c
Richard <rcezea007@gmail.com>
2024-06-30 14:40:53 +0100

    updated documentation

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-import express from 'express';
-import router from './routes/index';
-
+const express = require('express');
+const router = require('./routes/index');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 const app = express();
-
 app.use(express.json());
 app.use(router);
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
