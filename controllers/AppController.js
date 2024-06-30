eca3bf6fa95ce10ed68328161d7574bd30961c88
Richard <rcezea007@gmail.com>
2024-06-30 03:03:20 +0100

    update export

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,16 +1,18 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-export const getStatus = (req, res) => {
+const getStatus = (req, res) => {
   res.status(200).send({
     "redis": redisClient.isAlive(),
     "db": dbClient.isAlive()
   });
 };
 
-export const getStats = async (req, res) => {
+const getStats = async (req, res) => {
   res.status(200).send({
     "users": await dbClient.nbUsers(),
     "files": await dbClient.nbFiles()
   });
 };
+
+module.exports = { getStatus, getStats };
