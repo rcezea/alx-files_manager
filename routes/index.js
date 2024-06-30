18d9ac8f7d1e23a0dcded96c6f663f2fdcb42642
Richard <rcezea007@gmail.com>
2024-06-30 13:55:15 +0100

    updated documentation

--- routes/index.js
+++ routes/index.js
@@ -48,4 +48,4 @@ router.put('/files/:id/publish', FilesController.putPublish);
 // should set isPublic to false on the file document based on the ID
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
-module.exports = router;
+export default router;
