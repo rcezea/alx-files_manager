ab66d63bd69bb445383f0cfbb7e06f542ead6e19
Richard <rcezea007@gmail.com>
2024-06-30 03:39:23 +0100

    create, export and import class AppController

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,21 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-const getStatus = (req, res) => {
-  res.status(200).send({
-    "redis": redisClient.isAlive(),
-    "db": dbClient.isAlive()
-  });
-};
+class AppController {
+  getStatus = (req, res) => {
+    res.status(200).send({
+      "redis": redisClient.isAlive(),
+      "db": dbClient.isAlive()
+    });
+  };
 
-const getStats = async (req, res) => {
-  res.status(200).send({
-    "users": await dbClient.nbUsers(),
-    "files": await dbClient.nbFiles()
-  });
-};
+  getStats = async (req, res) => {
+    res.status(200).send({
+      "users": await dbClient.nbUsers(),
+      "files": await dbClient.nbFiles()
+    });
+  };
+}
 
-module.exports = { getStatus, getStats };
+
+module.exports = AppController;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import {getStatus, getStats} from '../controllers/AppController';
-import {postNew, getMe} from '../controllers/UsersController';
-import {getConnect, getDisconnect} from '../controllers/AuthController';
-import {postUpload, getShow, getIndex, putPublish, putUnpublish} from '../controllers/FilesController';
+import AppController from '../controllers/AppController';
+// import {postNew, getMe} from '../controllers/UsersController';
+// import {getConnect, getDisconnect} from '../controllers/AuthController';
+// import {postUpload, getShow, getIndex, putPublish, putUnpublish} from '../controllers/FilesController';
 
 const router = express.Router();
 
 
-router.get('/status', getStatus);
-router.get('/stats', getStats);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+/*
 router.post('/users', postNew);
 router.get('/connect', getConnect);
 router.get('/disconnect', getDisconnect);
@@ -18,5 +19,6 @@ router.get('/files/:id', getShow);
 router.get('/files', getIndex);
 router.put('/files/:id/publish', putPublish);
 router.put('/files/:id/unpublish', putUnpublish)
+*/
 
 module.exports = router;
