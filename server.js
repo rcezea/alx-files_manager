063da5ea90f472e8296665c3ec953cb3f2f55d1a
Richard <rcezea007@gmail.com>
2024-07-01 16:23:32 +0100

    adjusting dotenv import

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import controllerRouting from './routes/index';
 
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
 const port = Number(process.env.PORT) || 5000;
@@ -10,4 +11,3 @@ app.use(express.json({ limit: '200mb' }));
 controllerRouting(app);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-export default app;
